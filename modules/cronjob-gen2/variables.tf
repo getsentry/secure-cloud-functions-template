35435af21207aeee5e7c697ab7531092d6825f67
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2025-02-05 14:12:42 -0800

    remove fixed todo

--- modules/cronjob-gen2/variables.tf
+++ modules/cronjob-gen2/variables.tf
@@ -56,6 +56,4 @@ variable "attempt_deadline" {
 variable "deploy_sa_email" {
   type        = string
   description = "Service account used for CD in GitHub actions"
-  # TODO: Remove hardcoded account once deployment SA moved to terraform
-  default = "gha-cloud-functions-deployment@sec-dnr-infra.iam.gserviceaccount.com"
 }
